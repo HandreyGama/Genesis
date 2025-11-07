3fb99d3f35ee5d3ae95fd340bf67aeb92ff6819d
= <=>
2025-11-06 22:40:11 -0300

    Adicionando as pastas principais e os arquivos iniciais do projeto

---
+++ src/models/Exoplaneta.py
@@ -0,0 +1,10 @@
+class Exoplaneta():
+
+    raio:float;
+    temperatura:int;
+    quantidade_estrelas:int;
+
+    def __init__(self,raio,temperatura,quantidade_estrelas):
+        self.raio = raio;
+        self.temperatura = temperatura;
+        self.quantidade_estrelas = quantidade_estrelas;
