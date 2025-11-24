94475d7093054c5888d02c1ce8cbfc07b1913126
vitao-al <vm74531@gmail.com>
2025-11-24 18:27:44 -0300

    painel do planeta - comparar e saber mais

---
+++ src/frontend/static/js/painelComparar.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const planetSlides = document.querySelectorAll('.planet-slide');
+
+    planetSlides.forEach(slide => {
+        slide.addEventListener('click', (event) => {
+            const painel = slide.querySelector('.painel-planet');
+
+            painel.classList.toggle('mostrar');
+
+            event.stopPropagation();
+        });
+    });
+
+    document.addEventListener('click', (event) => {
+        const todosOsPaneis = document.querySelectorAll('.painel-planet');
+
+        todosOsPaneis.forEach(painel => {
+            if (painel.classList.contains('mostrar')) {
+
+                const cliqueDentroDoPainel = event.target.closest('.painel-planet');
+                const cliqueNoGatilho = event.target.closest('.planet-slide');
+
+                if (!cliqueDentroDoPainel && !cliqueNoGatilho) {
+                    painel.classList.remove('mostrar');
+                }
+            }
+        });
+    });
+});
